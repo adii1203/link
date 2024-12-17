c79bf263795b7d2594f092bd678016b2b50cf361
adii1203 <aditya32ft@gmail.com>
2024-12-17 19:55:33 +0530

    Initialize database and pass store to link handler

--- cmd/link/main.go
+++ cmd/link/main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
-	"github.com/adii1203/link/internal/handlers/link"
 	"log"
 	"net/http"
+
+	"github.com/adii1203/link/internal/handlers/link"
+	"github.com/adii1203/link/internal/initializers"
 )
 
 func main() {
+	store, err := initializers.New()
+	if err != nil {
+		log.Fatal("db initialization error")
+	}
+
 	router := http.NewServeMux()
 
-	router.HandleFunc("POST /api/links", link.New())
+	router.HandleFunc("POST /api/links", link.New(store))
 
 	server := http.Server{
 		Addr:    "localhost:3000",
 		Handler: router,
 	}
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal("error server starting")
 	}
