be418b1b1a59d9cf567ec2554524853dcf39907f
adii1203 <aditya32ft@gmail.com>
2024-12-17 21:42:22 +0530

    Add validation for base64 format and introduce Slug field to Link model

--- internal/types/types.go
+++ internal/types/types.go
@@ -3,4 +3,5 @@ package types
 type Link struct {
 	Id             int
 	DestinationUrl string `json:"destination_url" validate:"required,url"`
+	Slug           string `json:"slug" validate:"ascii"`
 }
