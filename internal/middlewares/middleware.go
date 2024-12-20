d635afa034d8857f2edeee8028636a233766e00b
adii1203 <aditya32ft@gmail.com>
2024-12-20 15:12:14 +0530

    Add IsCrawler middleware to identify and handle requests from known crawlers

--- internal/middlewares/middleware.go
+++ internal/middlewares/middleware.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/adii1203/link/internal/response"
 	"github.com/adii1203/link/internal/types"
@@ -32,5 +33,28 @@ func ValidatePayload(next http.Handler) http.Handler {
 
 		ctx := context.WithValue(r.Context(), "validatedPayload", link)
 		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func IsCrawler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userAgent := r.Header.Get("User-Agent")
+		crawlers := []string{
+			"twitterbot",
+			"facebookexternalhit",
+			"linkedinbot",
+			"googlebot",
+			"postmanruntime",
+		}
+
+		for _, crawler := range crawlers {
+			if strings.Contains(strings.ToLower(userAgent), crawler) {
+				// retunr html with metadata
+				ctx := context.WithValue(r.Context(), "isCrawler", "true")
+				next.ServeHTTP(w, r.WithContext(ctx))
+			}
+		}
+		next.ServeHTTP(w, r)
+
 	})
 }
